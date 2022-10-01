8f02b735f562f0c3844dc8deb2d6085cc240e30f
Denis Popov <idea-@list.ru>
2022-10-01 22:25:00 +0500

    All files

---
+++ src/BonusMilesService.java
@@ -0,0 +1,15 @@
+public class BonusMilesService {
+    public int Max(int a, int b) {
+        int price;
+        if (a > b) {
+            price = a;
+    } else {
+            price = b;
+    }
+        return price;
+    }
+
+    public int calculate(int price) {
+        return price;
+    }
+}
